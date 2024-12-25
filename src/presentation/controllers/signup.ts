3a398765ad047bc7761d0b0d476f0bee985a51cb
Pedro Igor <pedroicandido1@gmail.com>
2024-12-25 18:12:56 -0300

    feat: ensure signup controiller an error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,10 @@
 export default class SignUpController {
-  handle(httpReques: any): any {
-    return { statusCode: 400, body: new Error('Missing param: name') }
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return { statusCode: 400, body: new Error('Missing param: name') }
+    }
+    if (!httpRequest.body.email) {
+      return { statusCode: 400, body: new Error('Missing param: email') }
+    }
   }
 }
