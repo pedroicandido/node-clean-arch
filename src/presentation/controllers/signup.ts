3b360b027ae04de27edae37f6658d51cc1dd71f1
Pedro Igor <pedroicandido1@gmail.com>
2024-12-25 18:34:02 -0300

    refactor: ensure signup controller implements interface controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from "../errors/missing-param-error"
 import { badRequest } from "../helper/http-helper"
+import { IController } from "../protocols/controller"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
-export default class SignUpController {
+export default class SignUpController implements IController {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
