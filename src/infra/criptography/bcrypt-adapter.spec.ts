898d07a0da9266a00b84708590c057ca9df73524
Pedro Igor <pedroicandido1@gmail.com>
2024-12-28 16:24:34 -0300

    refactor: make sut creation to a method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,21 +1,26 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter';
+import { IEncrypter } from '../../data/protocols/encrypter';
 
 jest.mock('bcrypt', () => ({
   hashSync: () => 'hash'
 }))
 
+const salt = 12;
+
+const makeSut = (): IEncrypter => {
+  return new BcryptAdapter(salt);
+}
+
 test('Should call bcrypt with correct value', () => {
-  const salt = 12;
-  const sut = new BcryptAdapter(salt);
+  const sut = makeSut();
   const hasSpy = jest.spyOn(bcrypt, 'hashSync')
   sut.encrypt('any_value')
   expect(hasSpy).toHaveBeenCalledWith('any_value', salt)
 })
 
 test('Should return a hash on success', () => {
-  const salt = 12;
-  const sut = new BcryptAdapter(salt);
+  const sut = makeSut();
   const hash = sut.encrypt('any_value')
   expect(hash).toBe('hash')
 })
