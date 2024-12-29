6714ac0b472b45ebfc4afe8bf30f2d98ce697dac
Pedro Igor <pedroicandido1@gmail.com>
2024-12-29 17:15:58 -0300

    test: ensure mongo repository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import MongoHelper from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account  mongo repository', () => {
+  const mongoDbInstance = MongoHelper.getInstance();
+  beforeAll(async () => {
+    await mongoDbInstance.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await mongoDbInstance.close();
+  });
+
+
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository(mongoDbInstance)
+    const account = await sut.add({
+      name: "any_name",
+      email: 'any_email',
+      password: 'hashed_password'
+    })
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('hashed_password')
+  })
+})
