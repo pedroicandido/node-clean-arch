af1a3e17e4b3e4ecdeacaa7e172d25d0c314b068
Pedro Igor <pedroicandido1@gmail.com>
2024-12-31 17:38:25 -0300

    feat: ensure signup controller recives log repository

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,17 +2,22 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import SignUpController from "../../presentation/controllers/signup/signup";
-import { IController } from "../../presentation/protocols";
+import { HttpRequest, HttpResponse, IController } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import MongoHelper from "../../infra/db/mongodb/helpers/mongo-helper";
+import { LogControllerDecorator } from "../decorators/log";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
+
 
 
 export const makeSignupController = (): IController => {
   const mongoInstance = MongoHelper.getInstance();
+  const logErrorRepository = new LogMongoRepository(mongoInstance)
   const encrypter = new BcryptAdapter(12);
-  const addAccountRepository= new AccountMongoRepository(mongoInstance);
+  const addAccountRepository = new AccountMongoRepository(mongoInstance);
   const addAccount = new DbAddAccount(encrypter, addAccountRepository);
   const emailValidator = new EmailValidatorAdapter();
   const signUpController = new SignUpController(emailValidator, addAccount);
-  return signUpController;
+  const logControllerDecorator = new LogControllerDecorator(signUpController, logErrorRepository)
+  return logControllerDecorator;
 }
