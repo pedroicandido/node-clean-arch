5b7398435e222729f676b67493be7c44df791676
Pedro Igor <pedroicandido1@gmail.com>
2025-01-02 16:19:00 -0300

    refactor: move login validation to composite

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,32 +1,25 @@
-import { IAuthentication, IEmailValidator, HttpRequest, HttpResponse, IController } from "./login-protocols";
-import { InvalidParamError, MissingParamError } from "../../errors";
+import { IAuthentication, HttpRequest, HttpResponse, IController, IValidation } from "./login-protocols";
 import { badRequest, ok, serverError, unauthorized } from "../../helper/http-helper";
 
 export class LoginController implements IController {
-  private readonly emailValidator: IEmailValidator;
   private readonly authentication: IAuthentication;
-  constructor(emailValidator: IEmailValidator, authentication: IAuthentication) {
-    this.emailValidator = emailValidator;
+  private readonly validation: IValidation
+  constructor(validation: IValidation, authentication: IAuthentication) {
+    this.validation = validation;
     this.authentication = authentication;
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['email', 'password']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
       }
       const { email, password } = httpRequest.body
-      const isValid = this.emailValidator.isValid(email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
       const token = await this.authentication.auth(email, password)
       if (!token) {
         return unauthorized();
       }
-      return ok({token})
+      return ok({ token })
     } catch (error) {
       return serverError(error)
     }
