15e5b7f45db4f3f38f7ef9c8fe64d390e5d6726a
Pedro Igor <pedroicandido1@gmail.com>
2025-01-02 21:16:30 -0300

    feat: add login validation

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from "../../../presentation/helper/validators/validation-composite";
+import { RequiredFieldValidation } from "../../../presentation/helper/validators/required-field-validation";
+import { IValidation } from "../../../presentation/helper/validators/validation";
+import { EmailValidation } from "../../../presentation/helper/validators/email-validation";
+import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: IValidation[] = []
+  const requiredFields = ['email', 'password']
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  const validationComposite = new ValidationComposite(validations)
+  return validationComposite
+}
