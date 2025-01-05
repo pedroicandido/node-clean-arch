91efd0a47a82780edad3bfedbd369ccdaf534ac9
Pedro Igor <pedroicandido1@gmail.com>
2025-01-05 17:46:22 -0300

    feat: ensure account mongo repo returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,29 @@
 import { IAddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
+import { ILoadAccountEmailByRepository } from "../../../../data/protocols/db/load-account-email-by-repository";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import MongoHelper from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements IAddAccountRepository {
+export class AccountMongoRepository implements IAddAccountRepository, ILoadAccountEmailByRepository {
   private readonly mongoDbInstance: MongoHelper;
   constructor(mongoDbInstance: MongoHelper) {
     this.mongoDbInstance = mongoDbInstance;
   }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await this.mongoDbInstance.getCollection('accounts');
+    const account = await accountCollection.findOne({ email })
+    if(!account){
+      return null;
+    }
+    return {
+      email,
+      id: account._id.toString(),
+      name: account.name,
+      password: account.password
+    };
+  }
+
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await this.mongoDbInstance.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData)
